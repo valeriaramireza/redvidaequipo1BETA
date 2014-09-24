4a01141da657739ef53aa8519808679396955431
pedrox <pedrox@localhost>
2014-09-24 16:19:55 +0000

    Yo haciendo commit

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -9,7 +9,4 @@ $this->pageTitle=Yii::app()->name;
 <?php Yii::app()->user->setFlash(TbHtml::ALERT_COLOR_SUCCESS,'<strong>Well done!</strong> You successfully read this important alert message.');?>
 
 <img src="images/holders.jpg" width="1500" height="50">
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>Pedro modificó esto</p>
